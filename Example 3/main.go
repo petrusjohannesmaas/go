3febcdc15403ea3028841117cd5df322d2c904ff
PJ Maas <petrusjohannesmaas@outlook.com>
2024-07-18 11:25:23 +0200

    Setting up endpoints with Gorilla Mux and then requesting specific handles

--- Example 3/main.go
+++ Example 3/main.go
@@ -10,17 +10,15 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	}).Methods("GET")
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
 
-	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my about page!")
-	}).Methods("GET")
-
-	fs := http.FileServer(http.Dir("static"))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
 
 	fmt.Println("Server started on port 8080")
+	fmt.Println("Run 'curl http://localhost:8080/books/HarryPotter/page/1' to test endpoint")
 	http.ListenAndServe("localhost:8080", r)
 }
