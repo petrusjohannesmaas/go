999ad723049ef0a2fbc2431d2323359fff8a41b0
PJ <petrusjohannesmaas@outlook.com>
2024-07-23 20:18:26 +0200

    I wrote my first Go script without any documentation and it actually works lol

---
+++ Home_practise/practise.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func lineOne() {
+    var name string = "test"
+    fmt.Println("This is a ", name)
+}
+
+func lineTwo()  {
+    var difficulty bool = false                
+    fmt.Println("Is it easy? ", difficulty) 
+}
+
+func lineThree()  {
+    var verb string = "having"                   
+    fmt.Println("Am I", verb, "a good time?")
+}
+
+func lineFour() {
+    var percentage int = 100
+    fmt.Println("Yes,", percentage, "%")
+}
+
+func main() {
+    lineOne()
+    lineTwo()
+    lineThree()
+    lineFour()
+}
+
+
