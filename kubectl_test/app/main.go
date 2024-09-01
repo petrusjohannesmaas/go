15264db91733571ed86f900eaf913f313ed68c6b
Petrus Johannes <petrusjohannesmaas@outlook.com>
2024-09-01 11:39:37 +0200

    Trying to fix the server

--- kubectl_test/app/main.go
+++ kubectl_test/app/main.go
@@ -10,15 +10,15 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello human!")
+		io.WriteString(w, "Hello human! \n")
 		fmt.Println("A human has been greeted ✓")
 	})
 
 	mux.HandleFunc("/joke", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "your mom")
+		io.WriteString(w, "your mom \n")
 		fmt.Println("A joke has been made ✓")
 	})
 
-	fmt.Println("Server is running on port 80")
-	http.ListenAndServe("localhost:8080", mux)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
 }
