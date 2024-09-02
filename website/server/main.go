fcc2245289d1ab34c3bacc77394cfb33834f489a
PJ Maas <petrusjohannesmaas@outlook.com>
2024-09-02 19:04:19 +0200

    I need to route all the Coursera links in the backend to clean up the routing

---
+++ website/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "frontend/portfolio.html")
+	})
+
+	mux.HandleFunc("/style", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "frontend/style.css")
+	})
+
+	mux.HandleFunc("/sydney", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeContent(w, r, "frontend/style.css")
+	})
+
+	fmt.Println("Server is running on port 80")
+	http.ListenAndServe("localhost:8080", mux) // take out the localhost if you want to deploy with docker
+}
