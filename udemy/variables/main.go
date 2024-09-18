c6f0c12a0930b5094a723ea56efe3c8703f9fbb8
PJ Maas <petrusjohannesmaas@outlook.com>
2024-09-18 12:59:24 +0200

    Different ways to declare variables in Go

---
+++ udemy/variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare and then assign (two steps)
+	var firstNumber int
+	firstNumber = 2
+
+	// declare type and name and assign value
+	var secondNumber = 5
+
+	//declare name and value, let Go autoassign type
+	subtraction := 7
+
+	fmt.Println(firstNumber, secondNumber, subtraction)
+}
